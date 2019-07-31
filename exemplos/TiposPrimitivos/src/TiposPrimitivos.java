d83a6946fd9029be9008f6f20cc49f1506c701a4
Claudio Scheer <claudioscheer@protonmail.com>
2019-07-30 23:14:05 -0300

    Exemplo TiposPrimitivos

---
+++ exemplos/TiposPrimitivos/src/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lettuce
+ */
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        int numeroInteiro = 10;
+        long numeroLongo = 123456;
+        char caractere = '-';
+        boolean verdadeiroOuFalso = true;
+        byte b = 33; // -128 até 127 - 1 byte
+        float numeroComVirgula = (float) 10.5;
+        double numeroLongoComVirgula = (double) 12.5557;
+    }
+}
